3aa4128e57802686a466b68320813ec889370823
aditi <adiiig98@gmail.com>
2020-09-25 23:31:10 +0530

    ADITI_ASSIGNMENT

---
+++ assignments/Account/AccountBalanceLowException.java
@@ -0,0 +1,11 @@
+package com.hsbc.Account;
+
+public class AccountBalanceLowException extends Exception {
+
+	public AccountBalanceLowException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
