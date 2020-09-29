cbf775583336efa1ebc696b5a04d9bd28526ce17
bapatanuparth <parthanup@gmail.com>
2020-09-29 21:52:51 +0530

    Servlet-jsp Examples -3

---
+++ Book, Order servlet_Jsp Examples/book/Book.java
@@ -0,0 +1,40 @@
+package com.hsbc.servletdemo.book;
+
+public class Book {
+	private String bname;
+	private String author;
+	private String cat;
+	private int price;
+	public Book(String bname, String author, String cat, int price) {
+		super();
+		this.bname = bname;
+		this.author = author;
+		this.cat = cat;
+		this.price = price;
+	}
+	public String getBname() {
+		return bname;
+	}
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCat() {
+		return cat;
+	}
+	public void setCat(String cat) {
+		this.cat = cat;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
---
+++ Book, Order servlet_Jsp Examples/book/DBConnection.java
@@ -0,0 +1,32 @@
+package com.hsbc.servletdemo.book;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	
+	static Connection conn=null;
+	
+	public static Connection getConnection() {
+		try {
+			DriverManager.registerDriver(new org.apache.derby.jdbc.ClientDriver());
+
+			conn = DriverManager.getConnection("jdbc:derby://localhost:1527/TestDB2");
+			
+		}
+		catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return conn;
+	}
+	
+	public static void closeConnection() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
