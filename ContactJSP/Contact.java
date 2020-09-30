df1a68fa31df239215434009d51e54a828f3b661
rohancs22 <rohancs22@gmail.com>
2020-09-30 22:19:26 +0530

    Contact JSP
    Contact using JSP Assignment

---
+++ ContactJSP/Contact.java
@@ -0,0 +1,38 @@
+package com.hsbc.bean;
+
+public class Contact {
+	String first;
+	String last;
+	String email;
+	int phone;
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getLast() {
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+	@Override
+	public String toString() {
+		return "Contact [first=" + first + ", last=" + last + ", email=" + email + ", phone=" + phone + "]";
+	}
+	
+}
