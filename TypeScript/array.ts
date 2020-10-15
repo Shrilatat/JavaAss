1bb997e25562e65e546c75552ccb1359dd4218aa
poojagambhir <31560455+poojagambhir@users.noreply.github.com>
2020-10-15 20:54:35 +0530

    updated array.ts

--- TypeScript/array.ts
+++ TypeScript/array.ts
@@ -1,8 +1,8 @@
 var arr:number[]=[7,9,3,5];
 function arrOperation(arr:number[]){
-    let maximum:number =Math.max.apply(Math,arr);
+    let maximum:number =Math.max.apply(null,arr);
     console.log("Maximum number is: "+maximum);
-    let minimum:number =Math.min.apply(Math,arr);
+    let minimum:number =Math.min.apply(null,arr);
     console.log("Minimum number is: "+minimum);
     for (let index = 0; index < arr.length; index++) {
         const sum=0+arr[index];
