5eb5d63d2600c0c7429e74c5f9774332df1134c9
bharatk98 <42913366+bharatk98@users.noreply.github.com>
2020-10-16 15:55:00 +0530

    Add files via upload

---
+++ angular assignments/table-list-display/employee.ts
@@ -0,0 +1,13 @@
+export class employee{
+    empno;
+    ename;
+    salary;
+    desg;
+  
+    constructor(empno, ename, salary, desg){
+      this.desg=desg
+      this.empno = empno
+      this.ename = ename
+      this.salary = salary
+    }
+  }
