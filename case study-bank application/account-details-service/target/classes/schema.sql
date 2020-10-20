cd0193ddd3cf3bd8e9d09a693d6108d60b824e5b
bharatk98 <42913366+bharatk98@users.noreply.github.com>
2020-10-20 16:14:52 +0530

    SPRING BOOT MICROSERVICES

---
+++ case study-bank application/account-details-service/target/classes/schema.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS accounts;
+
+CREATE TABLE accounts (accId INT(8) NOT NULL,custId INT(8) NOT NULL,bal INT(8) NOT NULL);
---
+++ case study-bank application/bank-manager-service/src/main/java/com/hsbc/bankmanagerservice/resource/BankManagerServiceResource.java
@@ -0,0 +1,34 @@
+package com.hsbc.bankmanagerservice.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.hsbc.bankmanagerservice.model.Account;
+import com.hsbc.bankmanagerservice.model.Customer;
+import com.hsbc.bankmanagerservice.model.CustomerAccount;
+
+@RestController
+public class BankManagerServiceResource {
+
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping("/accounts/{custId}")
+	public CustomerAccount getCategories(@PathVariable("custId") int custId) {
+
+		Account account = restTemplate.getForObject("http://localhost:8081/accounts/" + custId, Account.class);
+		Customer customer = restTemplate.getForObject("http://localhost:8080/customers/" + custId, Customer.class);
+		System.out.println(customer);
+		System.out.println(account);
+		return new CustomerAccount(customer, account);
+
+	}
+}
