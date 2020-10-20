c0aa17035a6a27e324b4dd94a6844c0710083b50
poojagambhir <31560455+poojagambhir@users.noreply.github.com>
2020-10-20 19:44:56 +0530

    Case Study-Bank Application

---
+++ Bank Application/Customer-details-service/src/main/java/com/hsbc/customerdetailsservice/model/Customer.java
@@ -0,0 +1,37 @@
+package com.hsbc.customerdetailsservice.model;
+
+public class Customer {
+	int custId;
+	String custName;
+	String custAddress;
+	public Customer(int custId, String custName, String custAddress) {
+		super();
+		this.custId = custId;
+		this.custName = custName;
+		this.custAddress = custAddress;
+	}
+	public int getCustId() {
+		return custId;
+	}
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+	public String getCustName() {
+		return custName;
+	}
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	public String getCustAddress() {
+		return custAddress;
+	}
+	public void setCustAddress(String custAddress) {
+		this.custAddress = custAddress;
+	}
+	@Override
+	public String toString() {
+		return "Customer [custId=" + custId + ", custName=" + custName + ", custAddress=" + custAddress + "]";
+	}
+	
+	
+}
