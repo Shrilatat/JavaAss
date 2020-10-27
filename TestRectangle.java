0cdcadfd7182058b2ee6b6f486145e3250af2f80
ashwin-singhal <ashwin.singhal1306@gmail.com>
2020-10-27 09:32:25 +0530

    java assignment

---
+++ TestRectangle.java
@@ -0,0 +1,15 @@
+
+public class TestRectangle {
+	public static void main(String[] args) {
+		Rectangle r1 = new Rectangle(3,5);
+		Rectangle r2 = new Rectangle(8,10);
+		Rectangle r3 = new Rectangle(7,5);
+		Rectangle r4 = new Rectangle(15,20);
+		Rectangle r5 = new Rectangle(33,45);
+		System.out.println(r1);
+		System.out.println(r2);
+		System.out.println(r3);
+		System.out.println(r4);
+		System.out.println(r5);
+}
+}
