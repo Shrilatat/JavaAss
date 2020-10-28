18e18afa82298099b2fc78206cdda822c96854d4
TanuSingh14 <73451637+TanuSingh14@users.noreply.github.com>
2020-10-28 07:53:24 +0530

    Add files via upload
    Day1_Assignment

---
+++ TestRectangle.java
@@ -0,0 +1,20 @@
+
+public class TestRectangle {
+	public static void main(String[] args) {
+		Rectangle r1 = new Rectangle(4,2);
+		System.out.println(r1);
+		
+		Rectangle r2 = new Rectangle(5,2);
+		System.out.println(r2);
+		
+		Rectangle r3 = new Rectangle(14,10);
+		System.out.println(r3);
+		
+		Rectangle r4 = new Rectangle(9,5);
+		System.out.println(r4);
+		
+		Rectangle r5 = new Rectangle(10,11);
+		System.out.println(r5);
+		
+	}
+}
