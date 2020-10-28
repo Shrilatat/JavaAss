42174312c1e9968e48dd15b385707db19bde26a2
DARPAN <70426663+vdarpan@users.noreply.github.com>
2020-10-28 09:13:00 +0530

    Updated Empl Day2 Assignment

--- Empl.java
+++ Empl.java
@@ -7,26 +7,25 @@ public class Empl {
 	public Empl(String name, int sal) {
 		this.name = name;
 		this.sal = sal;
-		id++;
 	}
 	
-	public Empl() {id++;}
+	public Empl() {}
 	
 	public Empl(String name) {
 		this.name = name;
-		id++;
 	}
 	
 	public String toString() {
+		id++;
 		return "Empl [name=" + name + ", sal=" + sal + ", id=" + id +"]";
 	}
 
 	public static void main(String[] args) {
 		Empl E1= new Empl("Employee1", 10000);
-		System.out.println(E1);
 		Empl E2= new Empl("Employee2", 100000);
-		System.out.println(E2);
 		Empl E3= new Empl();
+		System.out.println(E1);
+		System.out.println(E2);
 		System.out.println(E3);
 	}
 	
