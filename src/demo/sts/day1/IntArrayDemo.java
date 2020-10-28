21184e224417d5d661e078eeacf7b9e80c6431e0
pikujs <pikujs@gmail.com>
2020-10-28 08:51:54 +0530

    day1 Ass updates
    day1 assignments almost complete

--- src/demo/sts/day1/IntArrayDemo.java
+++ src/demo/sts/day1/IntArrayDemo.java
@@ -1,8 +1,37 @@
 package demo.sts.day1;
 
+import java.util.Scanner;
+
 public class IntArrayDemo {
+	private static int arrLength = 5;
+	private static int[] intArray = new int[arrLength];
+	private static int[] sqArray = new int[arrLength];
+	
+	private static void scanElements() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter " + arrLength + " Integers(\r after each input): ");
+		for (int i=0;i<arrLength;i++)
+			intArray[i] = input.nextInt();
+		input.close();
+	}
+	
+	private int sumElements() {
+		int sum = 0;
+		for(int i : intArray)
+			sum += i;
+		return sum;
+	}
+	
+	private float avgElements() {
+		return sumElements()/intArray.length;
+	}
+	
+	private void squareElements() {
+		for (int i=0;i<intArray.length;i++)
+			sqArray[i] = intArray[i]^2;
+	}
+	
 	public static void main(String[] args) {
-		int[] intArray = new int[5];
 		
 	}
 }
--- src/demo/sts/day2/cmdArgs.java
+++ src/demo/sts/day2/cmdArgs.java
@@ -2,8 +2,7 @@ package demo.sts.day2;
 
 public class cmdArgs {
 	public static void main(String[] args) {
-		String message = args[0];
 		for (int i=0;i<Integer.parseInt(args[1]);i++)
-			System.out.println(message);
+			System.out.println(args[0]);
 	}
 }
