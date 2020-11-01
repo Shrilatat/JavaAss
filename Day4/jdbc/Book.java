15ea309f316b4efe77565445aa6f1b6809bed834
Aayush Kumar <ch16btech11002@iith.ac.in>
2020-11-01 11:54:43 +0530

    Updated

---
+++ Day4/jdbc/Book.java
@@ -0,0 +1,42 @@
+package Day4;
+
+public class Book {
+	private int bookid;
+	private String bname, author;
+	private double price;
+
+	public String toString() {
+		return "Book [bookid=" + bookid + ", bname=" + bname + ", author=" + author + ", price=" + price + "]";
+	}
+	public int getBookid() {
+		return bookid;
+	}
+	public void setBookid(int bookid) {
+		this.bookid = bookid;
+	}
+	public String getBname() {
+		return bname;
+	}
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public Book(int bookid, String bname, String author, double price) {
+		super();
+		this.bookid = bookid;
+		this.bname = bname;
+		this.author = author;
+		this.price = price;
+	}
+}
